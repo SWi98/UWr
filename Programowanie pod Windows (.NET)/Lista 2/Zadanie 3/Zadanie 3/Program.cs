34d4d8b2c226470d0268d1f3948619918727f279
Szymon <39309167+BeoSz@users.noreply.github.com>
2019-03-25 23:45:11 +0100

    Zad3, Zad4

---
+++ Programowanie pod Windows (.NET)/Lista 2/Zadanie 3/Zadanie 3/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// Zademnostrować metody ConvertAll, FindAll, ForEach, RemoveAll i Sort z klasy List<T>
+// korzystając z anonimowych delegacji
+namespace Zadanie_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> test_list = new List<int>();
+
+            Console.WriteLine("Domyslna lista:");
+            for (int i = 0; i < 10; i++)
+            {
+                test_list.Add(i * 4);
+            }
+            writeList(test_list);
+
+            List<string> test_list_string = test_list.ConvertAll(
+                new Converter<int, string>(delegate (int x)
+                {
+                    return x.ToString() + " string";
+                }));
+
+            Console.Write("\n");
+            Console.WriteLine("Przekonwertowana na typ string:");
+
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine(test_list_string[i]);
+            }
+
+            List<int> test_findall = test_list.FindAll(delegate (int x)
+            {
+                return x % 6 == 0;
+            });
+
+            Console.Write("\n");
+            Console.WriteLine("Nowa lista, zlozona z elementow " +
+                "podzielnych przez 6 (FindAll):");
+            writeList(test_findall);
+            Console.Write("\n");
+            Console.WriteLine("Wypisanie elementow podzielonych przez 10 korzystajac" +
+                " z ForEach:");
+
+            test_list.ForEach(delegate (int x)
+            {
+                if(x % 10 == 0)
+                    Console.WriteLine(x);
+            });
+
+            test_list.RemoveAll(delegate (int x)
+            {
+                return x > 10;
+            });
+
+            Console.Write("\n");
+            Console.WriteLine("Usuniecie elementow wiekszych niz 10 uzywajac DeleteAll:");
+            writeList(test_list);
+            Console.WriteLine("\nPosortowanie malejaco uzywajac .Sort():");
+
+            test_list.Sort(delegate (int x, int y)
+            {
+                if (x == y)
+                    return 0;
+                else if (x > y)
+                    return -1;
+                else
+                    return 1;
+            });
+
+            writeList(test_list);
+            Console.ReadKey();
+        }
+        
+        static void writeList(List<int> list)
+        {
+            for (int i = 0; i < list.Count(); i++)
+                Console.WriteLine(list[i]);
+        }
+    }
+}
---
+++ Programowanie pod Windows (.NET)/Lista 2/Zadanie4/Zadanie4/Program.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadanie4
+{
+    class ListHelper
+    {
+        public static List<TOutput> ConvertAll<T, TOutput>(List<T> list, 
+            Converter<T, TOutput> converter)
+        {
+            List<TOutput> new_list = new List<TOutput>();
+            for(int i = 0; i < list.Count(); i++)
+            {
+                new_list.Add(converter(list[i]));
+            }
+
+            return new_list;
+        }
+        
+        public static List<T> FindAll<T>(List<T> list, Predicate<T> match)
+        {
+            List<T> new_list = new List<T>();
+            for(int i = 0; i < list.Count(); i++)
+            {
+                if (match(list[i]))
+                {
+                    new_list.Add(list[i]);
+                }
+            }
+
+            return new_list;
+        }
+
+        public static void ForEach<T>(List<T> list, Action<T> action)
+        {
+            for(int i = 0; i < list.Count(); i++)
+            {
+                action(list[i]);
+            }
+        }
+
+        public static int RemoveAll<T>(List<T> list, Predicate<T> match)
+        {
+            int Counter = 0;
+            int n = list.Count();
+
+            for (int i = 0; i < n; i++)
+            {
+                if (match(list[i]))
+                {
+                    list.RemoveAt(i);
+                    i--;
+                    n--;
+                    Counter += 1;
+                }
+            }
+
+            return Counter;
+        }
+
+
+        public static void Sort<T>(List<T> list, Comparison<T> comparison)
+        {
+            for(int i = 0; i < list.Count(); i++)
+            {
+                int k = i;
+                for(int j = i+1; j < list.Count(); j++)
+                {
+                    if(comparison(list[j], list[k]) < 0)
+                    {
+                        k = j;
+                        T tmp = list[j];
+                        list[j] = list[k];
+                        list[k] = tmp;
+                    }
+                }
+
+            }
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
