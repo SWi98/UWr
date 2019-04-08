4a911687093da7fb2be57b5465f4e9a35ab2d8fa
Szymon <39309167+BeoSz@users.noreply.github.com>
2019-04-08 19:07:40 +0200

    Lista4 .Net

---
+++ Programowanie pod Windows (.NET)/Lista 4/Zadanie1/Zadanie1/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadanie1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int Foo(int x, int y)
+            {
+                return x * x * x * x * x * x * x * x * x * x * y * y + 2 * x + 2 * y / (x + 5 * y) +
+                     x * x * x * x * x * x * x * x * x * x * y * y + 2 * x + 2 * y / (x + 5 * y) +
+                     x * x * x * x * x * x * x * x * x * x * y * y + 2 * x + 2 * y * (x * x * x * x * x * x * x * x * x * x * y * y + 2 * x + 2 * y / (x + 5 * y) +
+                     x * x * x * x * x * x * x * x * x * x * y * y + 2 * x + 2 * y / (x + 5 * y) +
+                     x * x * x * x * x * x * x * x * x * x * y * y + 2 * x + 2 * y) / (x * x * x * x * x * x * x * x * x * x * y * y + 2 * x + 2 * y / (x + 5 * y) +
+                     x * x * x * x * x * x * x * x * x * x * y * y + 2 * x + 2 * y / (x + 5 * y));
+            }
+
+            dynamic Foo2(dynamic x, dynamic y)
+            {
+                return x * x * x * x * x * x * x * x * x * x * y * y + 2 * x + 2 * y / (x + 5 * y) +
+                     x * x * x * x * x * x * x * x * x * x * y * y + 2 * x + 2 * y / (x + 5 * y) +
+                     x * x * x * x * x * x * x * x * x * x * y * y + 2 * x + 2 * y * (x * x * x * x * x * x * x * x * x * x * y * y + 2 * x + 2 * y / (x + 5 * y) +
+                     x * x * x * x * x * x * x * x * x * x * y * y + 2 * x + 2 * y / (x + 5 * y) +
+                     x * x * x * x * x * x * x * x * x * x * y * y + 2 * x + 2 * y) / (x * x * x * x * x * x * x * x * x * x * y * y + 2 * x + 2 * y / (x + 5 * y) +
+                     x * x * x * x * x * x * x * x * x * x * y * y + 2 * x + 2 * y / (x + 5 * y));
+            }
+
+
+            for(int i = 0; i < 10; i++)
+            {
+                DateTime Start = DateTime.Now;
+                for (int j = 0; j < 1000000; j++)
+                {
+                    Foo(2147483647, 2147483647);
+                }
+                DateTime End = DateTime.Now;
+                Console.WriteLine(End - Start);
+
+                Start = DateTime.Now;
+                for (int j = 0; j < 1000000; j++)
+                {
+                    Foo2(2147483647, 2147483647);
+                }
+                End = DateTime.Now;
+                Console.WriteLine(End - Start);
+
+                Console.WriteLine("-------");
+            }
+            Console.ReadKey();
+        }
+    }
+}
