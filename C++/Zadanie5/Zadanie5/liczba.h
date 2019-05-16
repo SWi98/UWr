329a11708561a84f623fd98ac663ad42695d7751
Szymon <wieczorek0725@gmail.com>
2019-05-16 15:10:23 +0200

    polecenia

--- C++/Zadanie5/Zadanie5/liczba.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include "wyrazenie.h"
-class liczba : wyrazenie {
-public:
-	double get();
-	void set();
-private:
-	double val; 
-};
--- C++/Zadanie5/Zadanie5/stala.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-#include "wyrazenie.h"
-class stala : wyrazenie {
-public:
-	double get();
-	void set();
-};
