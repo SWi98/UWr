75c017b48f7ddf38af5bf12dbd5b77c99e6fed91
SWi98 <wieczorek0725@gmail.com>
2019-10-10 15:17:22 +0200

    cpp p

--- Python Rozszerzony/Lista 1/Lista1.py
+++ Python Rozszerzony/Lista 1/Lista1.py
@@ -1,15 +1,15 @@
 import functools
 import itertools
 
-def sum23(a, b):
-    return round(a + 0.77 * b, 2)
-
-def vat_faktura(zakupy): 
-    return functools.reduce(sum23, zakupy)
+def vat_faktura(zakupy):
+    sum = 0
+    for x in zakupy:
+        sum += 0.77 * x
+    return sum
 
 def vat_paragon(zakupy):
-    x = sum(zakupy)
-    return round(0.77 * x, 2)
+    return 0.77 * sum(zakupy)
+    
 
 def drobne(kwota):
     noms = {1 : 0, 2 : 0, 5 : 0, 10 : 0, 20: 0}
@@ -34,15 +34,6 @@ def romb(n):
         print(" " * spaces +  '#' * h)
         h -= 2
         spaces += 1
-        
-
-#def zaszyfruj(tekst, klucz):
- #   res = ""
-  #  bin_klucz = bin(klucz)
-   # for i in range(len(tekst)):
-    #    ascii_num = ord(tekst[i])
-     #   bin_num = bin(ascii_num)
-
 
 
 print(vat_faktura([1, 2, 3, 4, 20.5, 115.29]))
@@ -50,6 +41,5 @@ print(vat_paragon([1, 2, 3, 4, 20.5, 115.29]))
 drobne(19)
 drobne(7)
 drobne(51)
-romb(1)
-romb(5)
-romb(10)
+x = int(input())
+romb(x)
