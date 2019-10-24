cd15f285e6b85cdcd9d6c27f3932c39bc8a5562a
SWi98 <wieczorek0725@gmail.com>
2019-10-24 15:11:25 +0200

    cpp

--- C++ Rozszerzony/Lista 3/Zad4.cpp
+++ C++ Rozszerzony/Lista 3/Zad4.cpp
@@ -5,10 +5,11 @@ using namespace std;
 
 template<typename A, typename B, typename enable_if<is_pointer<B>::value>::type* = nullptr>
 void move(A &new_ob, B source, typename enable_if<is_convertible<decltype(*source), A>::value>::type* = nullptr){
+    //if(is_convertible<decltype(*source), A>::value)
     new_ob = (A)*source; 
 }
 
-template<typename A, typename B, typename enable_if<!is_pointer<B>::value && is_convertible<A, B>::value>::type* = nullptr>
+template<typename A, typename B, typename enable_if<is_convertible<A, B>::value>::type* = nullptr>//typename enable_if<!is_pointer<B>::value && is_convertible<A, B>::value>::type* = nullptr>
 void move(A &new_ob, B source){
     if(is_convertible<decltype(source), A>::value)
         new_ob = (A)source;
--- C++ Rozszerzony/Lista 3/Zad5.cpp
+++ C++ Rozszerzony/Lista 3/Zad5.cpp
@@ -1,7 +1,45 @@
 #include <iostream>
-
+#include <vector>
+#include <chrono>
 using namespace std;
 
+void fill_vector(vector<vector<double>> &matrix){
+    for(int i = 0; i < matrix.size(); i++){
+        for(int j = 0; j < matrix[i].size(); j++){
+            double random = (rand() % 16 + 5) / 10;
+            matrix[i][j] = random;
+        }
+    }
+}
+
+vector<vector<double>> mult(vector<vector<double>> &matrix){
+    int N = matrix.size();
+    vector<vector<double>> new_matrix(N, vector<double>(N));
+    for(int i = 0; i < N; i++){
+        for(int j = 0; j < N; j++){
+            new_matrix[i][j] = 0;
+            for(int k = 0; k < N; k++){
+                new_matrix[i][j] += matrix[i][k] * matrix[k][j];
+            }
+        }
+    }
+    return new_matrix; 
+}
+
 int main(){
-    
+    srand(time(NULL));
+    vector<vector<double>> matrix10(10, vector<double>(10));
+    vector<vector<double>> matrix100(100, vector<double>(100));
+    vector<vector<double>> matrix1000(1000, vector<double>(1000));
+    vector<vector<double>> matrix10000(10000, vector<double>(10000));
+    fill_vector(matrix10); fill_vector(matrix100); fill_vector(matrix1000); fill_vector(matrix10000);
+
+    chrono::duration<double> sum(0);
+    for(int i = 0; i < 10; i++){
+        auto start = chrono::high_resolution_clock::now();
+        matrix100 = mult(matrix100);
+        auto end = chrono::high_resolution_clock::now();
+        sum += chrono::duration_cast<chrono::duration<double>>(end - start);
+    }
+    cout << sum.count() / 10 << endl;
 }
