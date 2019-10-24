f643d5e259860c4e07f8b311397254030dd5572c
SWi98 <wieczorek0725@gmail.com>
2019-10-24 12:49:04 +0200

    zad4

--- C++ Rozszerzony/Lista 3/Zad4.cpp
+++ C++ Rozszerzony/Lista 3/Zad4.cpp
@@ -3,15 +3,14 @@
 #include <type_traits>
 using namespace std;
 
-template<typename A, typename B, typename enable_if<is_pointer<B>::value, B>::type* = nullptr>
-void move(A &new_ob, B source){
-    if(is_convertible<typeof(*source), A>::value)
-        new_ob = (A)*source; 
+template<typename A, typename B, typename enable_if<is_pointer<B>::value>::type* = nullptr>
+void move(A &new_ob, B source, typename enable_if<is_convertible<decltype(*source), A>::value>::type* = nullptr){
+    new_ob = (A)*source; 
 }
 
-template<typename A, typename B, typename enable_if<!is_pointer<B>::value, B>::type* = nullptr>
+template<typename A, typename B, typename enable_if<!is_pointer<B>::value && is_convertible<A, B>::value>::type* = nullptr>
 void move(A &new_ob, B source){
-    if(is_convertible<typeof(source), A>::value)
+    if(is_convertible<decltype(source), A>::value)
         new_ob = (A)source;
 }
 
@@ -27,4 +26,8 @@ int main(){
 
     move(z2, a);
     cout << z2 << endl;
+
+    string str = "asdasd";
+    //move(a, str);
+
 }
