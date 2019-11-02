4a367c263eeebd86dce7a069dbc50fddbdafa859
Szymon <wieczorek0725@gmail.com>
2019-11-02 13:56:52 +0100

    weppo 3.2

---
+++ WEPPO/Lista3/Zad2.js
@@ -0,0 +1,104 @@
+function rec_fib(n){
+    if (n == 1 || n == 0){
+        return n;
+    }
+    return rec_fib(n-1) + rec_fib(n-2);
+}
+
+function iter_fib(n){
+    var a = 0, b = 1;
+    for (var i = 2; i <= n; i ++){
+        b = a + b;
+        a = b - a;
+    }
+    return b;
+}
+
+function rec_mem_fib(n){
+    var cache = {};
+
+    function _fib(n){
+        if(n in cache){
+            return cache[n];
+        }
+        else if(n == 0 || n == 1){
+            cache[n] = n;
+            return cache[n];
+        }
+        else{
+            cache[n] = _fib(n-1) + _fib(n-2);
+            return cache[n];
+        }
+    }
+
+    return _fib(n);
+}
+
+function auto_mem(fn){
+    var cache = {};
+    var slice = Array.prototype.slice;
+    return function(n){
+        var args = slice.call(arguments);
+        if(args in cache){
+            return cache[args];
+        }
+        else{
+            cache[args] = fn.apply(this, args);
+            return cache[args];
+        }
+    }
+}
+
+function test_rec(n){
+    console.log("RECURSIVE");
+    for(var i = 1; i <= n; i++){
+        console.time();
+        console.log("\n", "fib(", i, ") =", rec_fib(i));
+        console.timeEnd();
+    }
+    console.log("\n\n");
+}
+
+function test_iter(n){
+    console.log("ITERATIVE");
+    for(var i = 1; i <= n; i++){
+        console.time();
+        console.log("\n", "fib(", i, ") =", iter_fib(i));
+        console.timeEnd();
+    }
+    console.log("\n\n");
+}
+
+function test_mem(n){
+    console.log("MEMOIZATION");
+    for(var i = 1; i <= n; i++){
+        console.time();
+        console.log("\n", "fib(", i, ") =", rec_mem_fib(i));
+        console.timeEnd();
+    }
+    console.log("\n\n");
+}
+
+function test_auto_mem(n){
+    console.log("AUTO MEMOIZATION");
+    let foo = auto_mem(rec_fib);
+    for(var i = 1; i <= n; i++){
+        console.time();
+        console.log("\n", "fib(", i, ") =", foo(i));
+        console.timeEnd();
+    }
+    console.log("__________________");
+    for(var i = 1; i <= n; i++){
+        console.time();
+        console.log("\n", "fib(", i, ") =", foo(i));
+        console.timeEnd();
+    }
+    console.log("\n\n");
+}
+
+test_rec(42);
+test_iter(42);
+test_mem(42);
+test_auto_mem(42);
+
+//IE dla 35
