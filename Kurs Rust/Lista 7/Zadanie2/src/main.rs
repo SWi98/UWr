889c9cb93d06bf6fd626d7b6aa7ce224fb749f57
SWi98 <wieczorek0725@gmail.com>
2019-11-21 22:36:36 +0100

    rust

--- Kurs Rust/Lista 7/Zadanie2/src/main.rs
+++ Kurs Rust/Lista 7/Zadanie2/src/main.rs
@@ -2,7 +2,7 @@ use std::collections::BTreeMap;
 
 fn letter_frequency(input: &str) -> BTreeMap<char, i32> {
     let mut btree = BTreeMap::new();
-    for c in input.to_string().chars().filter(|x| *x != ' ' && x.is_alphanumeric()).collect::<String>().to_lowercase().chars(){
+    for c in input.to_string().chars().filter(|x| x.is_alphabetic()).collect::<String>().to_lowercase().chars(){
         *btree.entry(c).or_insert(0) += 1;
     }
     return btree;
