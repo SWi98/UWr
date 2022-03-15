70a9d92dad364aa2736975294ee387d1aa77e0b0
SWi <wieczorek0725@gmail.com>
2022-03-15 11:23:02 +0100

    TfWB - List 2 - Task 1

---
+++ Tools for Web Development/List 2/task1-app/src/App.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+import Logo from './Logo';
+import HelloWorld from './HelloWorld'
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Logo />
+        <HelloWorld />
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ Tools for Web Development/List 2/task1-app/src/HelloWorld.tsx
@@ -0,0 +1,14 @@
+import React from "react"
+
+class HelloWorld extends React.Component{
+  render() {
+    return (
+      <div>
+        <p style={{display: "inline"}}>Hello </p>
+        <p style={{display: "inline"}}>World!</p>
+      </div>
+    );
+  }
+}
+
+export default HelloWorld
